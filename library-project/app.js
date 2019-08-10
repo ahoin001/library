db92db6eedc67899ae49617ff8bb5344cdb5c8af
Alexander Hoinville <ahoin001@gmail.com>
2019-08-10 15:18:36 -0400

    Update Documentation

--- library-project/app.js
+++ library-project/app.js
@@ -46,8 +46,6 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
@@ -57,8 +55,6 @@ const index = require('./routes/index');
 app.use('/', index);
 
 // connect authorRoutes with app.js
-// const authorRoutes = require("./routes/author-routes");
-// app.use("/", authorRoutes);
 app.use("/", require("./routes/author-routes"));
 
 
--- library-project/routes/book-router.js
+++ library-project/routes/book-router.js
@@ -2,35 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 // require Author model in order to use it for CRUD
-const Author = require("../models/Author");
+const Book = require("../models/Book");
 
 // GET - to display the form for Creating the authors
-router.get("/authors/new", (req, res, next) => {
+router.get("/books/new", (req, res, next) => {
   // make sure yoy see all the folders that are inside the "views" folder,
   // you don't have to specify "views" folder tho
-  res.render("author-views/new-author");
+  res.render("book-views/new-book");
 });
-
-router.get("/authors", (req, res, next) => {
-  Author
-    .find()
-    .then((authorsFromDB) => res.render("author-views/all-authors", { authors: authorsFromDB }))
-    .catch(err => console.log(`Error while getting authors from the DB: `, err))
-
-})
 
 // post route to create a new author in the DB
-{/* <form action="/authors/create" method="post"> */ }
-router.post("/authors/create", (req, res, next) => {
+{/* <form action="/authors/create" method="post"> */}
+router.post("/books/create", (req, res, next) => {
   // console.log("THE FORM: ", req.body);
-  Author
+  Book
     .create(req.body)
-    .then(newAuthor => console.log("NEW AUTHOR: ", newAuthor))
+    .then( newBook => console.log("NEW AUTHOR: ", newBook) )
     .catch(err => console.log("Error while creating a new author: ", err));
+
 
 });
 
+router.get("/books", (req,res,next) => {
+  Author
+  .find()
+  .then((booksFromDB) =>  res.render("book-views/allBooks", {books:booksFromDB}) )
+  .catch(err => console.log(`Error while getting authors from the DB: `,err ))
 
+})
 
 
 // in order to use routes anywhere else in this application, we have to export them
